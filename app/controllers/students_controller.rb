50ff37352cb1edf6d281ba4df4d0936156b6e1e7
George Ruiz <georuiz817@gmail.com>
2019-08-27 18:04:50 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,7 +12,12 @@ class StudentsController < ApplicationController
   end
 
   def create
-    
+      @student = Student.new
+2.	  @student. = params[:title]
+3.	  @post.description = params[:description]
+4.	  @post.save
+5.	  redirect_to post_path(@post)
+
   end
 
 end
