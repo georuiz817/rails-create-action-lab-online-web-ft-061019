f3a13f39bd863d9999c25431a6ee56aa4bbba6f5
George Ruiz <georuiz817@gmail.com>
2019-08-27 18:05:15 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,7 +13,7 @@ class StudentsController < ApplicationController
 
   def create
       @student = Student.new
-2.	  @student. = params[:title]
+2.	  @student.first_name = params[:first_name]
 3.	  @post.description = params[:description]
 4.	  @post.save
 5.	  redirect_to post_path(@post)
